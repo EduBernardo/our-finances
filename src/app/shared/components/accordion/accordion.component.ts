9fc3ecb7d58eb0d4152a18352c5347b951137966
Edu Bernardo <eduluizbernardo@gmail.com>
2022-03-03 23:12:28 -0300

    feat: inclusao dos icones do accordion

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordion.component.scss']
 })
 export class AccordionComponent implements OnInit {
-  items = ['Item 1', 'Item 2', 'Item 3'];
+  items = [
+    {id:'itau', name:'Itaú', imgSrc:'../../../../assets/img/banks/itau-logo.png', purpose:'Previdência'},
+    {id:'picpay', name:'PicPay', imgSrc:'../../../../assets/img/banks/picpay-logo.png', purpose:'Fundo de Emergência'},
+    {id:'mercadoPago', name:'Mercado Pago', imgSrc:'../../../../assets/img/banks/mercado-pago-logo.png', purpose:'Fundo para Casa Própria'},
+    {id:'nuBank', name:'Nu Bank', imgSrc:'../../../../assets/img/banks/nu-bank-logo.png', purpose:'Fundo de Emergência Mushu'},
+    {id:'nuInvest', name:'Nu Invest', imgSrc:'../../../../assets/img/banks/nu-invest-logo.png', purpose:'Investimentos Bolsa de Valores'},
+    {id:'mercadoBitcoin', name:'Mercado Bitcoin', imgSrc:'../../../../assets/img/banks/mercado-bitcoin-logo.png', purpose:'Investimentos Crypto'},
+
+
+  ];
   expandedIndex = 0;
   
   constructor() { }
