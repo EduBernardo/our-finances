540d800b23068a8231c6462ccfd088a423010c8b
Edu Bernardo <eduluizbernardo@gmail.com>
2022-08-20 11:32:01 -0300

    integracao com github actions

--- src/app/investiments-dashboard/investiments-dashboard.component.ts
+++ src/app/investiments-dashboard/investiments-dashboard.component.ts
@@ -12,13 +12,13 @@ export class InvestimentsDashboardComponent implements OnInit {
   userID: any;
   chartUrl: string;
   banks = [
-    {id:'itau', name:'Itaú', imgSrc:'../../../../assets/img/banks/itau-logo.png', purpose:'Fundo de Emergência + Previdência', value:19843.37},
-    {id:'picpay', name:'PicPay', imgSrc:'../../../../assets/img/banks/picpay-logo.png', purpose:'Fundo para Casa Própria', value:10884.97},
-    {id:'mercadoPago', name:'Mercado Pago', imgSrc:'../../../../assets/img/banks/mercado-pago-logo.png', purpose:'Fundo de Emergência Mushu', value:45.94},
-    {id:'nuBank', name:'Nu Bank', imgSrc:'../../../../assets/img/banks/nu-bank-logo.png', purpose:'Fundo de Emergência Mushu', value:14.38},
+    {id:'itau', name:'Itaú', imgSrc:'../../../../assets/img/banks/itau-logo.png', purpose:'Fundo de Emergência + Previdência', value:21187.95},
+    {id:'picpay', name:'PicPay', imgSrc:'../../../../assets/img/banks/picpay-logo.png', purpose:'Fundo para Casa Própria', value:10003.96},
+    {id:'mercadoPago', name:'Mercado Pago', imgSrc:'../../../../assets/img/banks/mercado-pago-logo.png', purpose:'Fundo de Emergência Mushu', value:67.39},
+    {id:'nuBank', name:'Nu Bank', imgSrc:'../../../../assets/img/banks/nu-bank-logo.png', purpose:'Fundo de Emergência Mushu', value:25.81},
     {id:'nuInvest', name:'Nu Invest', imgSrc:'../../../../assets/img/banks/nu-invest-logo.png', purpose:'Investimentos Bolsa de Valores', value:4231.68},
-    {id:'mercadoBitcoin', name:'Mercado Bitcoin', imgSrc:'../../../../assets/img/banks/mercado-bitcoin-logo.png', purpose:'Investimentos Crypto', value:52.21},
-    {id:'caixa', name:'Caixa', imgSrc:'../../../../assets/img/banks/caixa-logo.png', purpose:'Fundo de Garantia', value:26903.93},
+    {id:'mercadoBitcoin', name:'Mercado Bitcoin', imgSrc:'../../../../assets/img/banks/mercado-bitcoin-logo.png', purpose:'Investimentos Crypto', value:57.25},
+    {id:'caixa', name:'Caixa', imgSrc:'../../../../assets/img/banks/caixa-logo.png', purpose:'Fundo de Garantia', value:27556.09},
 
   ];
 
