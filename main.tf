d74a24bff79bdf5512ed6469de5d29fa8de7e314
Edu Bernardo <eduluizbernardo@gmail.com>
2024-01-27 02:21:35 -0300

    infra: ajuste do terraform

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-      backend "remote" {
-        organization = "dedronlabdevopscloud"
-
-        workspaces {
-          name = "devopslab-infra"
-        }
-      }
-    }
-
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-    }
+resource "aws_s3_bucket" "example" {
+  bucket = "our-finances-s3"
+}
